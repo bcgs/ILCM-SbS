64bceeaeac26feb94d7f52fdfe75d7b1921f7781
helmitonjunior <hmscj@cin.ufpe.br>
2016-10-13 23:17:42 -0300

    Fixed ICML, now working with slight error margin

--- src/Simulator.java
+++ src/Simulator.java
@@ -6,9 +6,12 @@ public class Simulator {
 	 * refer to ~pasg/if740/Projeto-RFID.pdf
 	 * for more information
 	 */
+	
+	int[] slots;
+	
 	public int[] genRandom(double n, int frameSize){
-		int[] slots = new int[frameSize];
-
+		
+		slots = new int[frameSize];
 		// status[] represents [e|s|c]
 		// e = number of empty slots
 		// s = number of success slots
@@ -32,7 +35,7 @@ public class Simulator {
 	public void simulate(String protocol, double numTags, double incr, int maxTags, int eval, int fmSize) {
 		System.out.println("===== "+protocol+" =====");
 		System.out.println("TAGS\tAV[e|s|c]\tTIME\tSLOTS\tERROR\n");
-		
+
 		// We need to test for 100, 200, 300, ..., 1000 tags
 		for (double env = 1; env <= maxTags/numTags; env+=incr/numTags) {
 			
@@ -47,6 +50,8 @@ public class Simulator {
 				long start = System.currentTimeMillis();
 				
 				double n = numTags*env; //Number of tags
+				
+				
 				int L = fmSize; //scurrent frame size
 
 				// Tell tags they can choose their own random number
@@ -84,7 +89,8 @@ public class Simulator {
 					c += collision;
 
 					// Required by ILCM. 2^qc = L
-					int qc = log2(L);
+					int qc = (int) Math.ceil(log2(L));
+					//System.out.println("QCurrent= "+ qc);
 
 					/* The estimators are going to estimate
 					 * how many tags (n_) would have been involved
@@ -92,7 +98,7 @@ public class Simulator {
 					 * of number of tags which should taken as the
 					 * next frame size. Quite obvious, isn't it?
 					 */
-					int f_ = (int) Math.ceil(estimateFunction(protocol,qc,empty,success,collision));
+					int f_ = (int) Math.ceil(estimateFunction(protocol,qc,collision, slots));
 					L = f_;
 
 					if(!flag) {
@@ -117,8 +123,8 @@ public class Simulator {
 		}
 	}
 
-	public int log2(double x) {
-		return (int) (Math.log(x)/Math.log(2));
+	public double log2(double x) {
+		return (Math.log(x)/Math.log(2));
 	}
 
 	public double _N(double k, int s, double l) {
@@ -126,41 +132,60 @@ public class Simulator {
 	}
 
 	public double _R(double N, int qc, int i) {
-		return (N * Math.pow(2, qc) / i);
+		return (N * Math.pow(2, qc))/i;
 	}
 
-	public double estimateFunction(String function, int qc, int e, int s, int c) {
+	public double estimateFunction(String function, int qc, int C, int[] frame) {
 		switch (function) {
 		case "lower-bound":
-			return c*2;
+			return C*2;
 		case "schoute":
-			return c*2.39;
+			return C*2.39;
 		case "ilcm-sbs":
 			int i = 0;
 			double qn = -1;
-
-			while(qn == -1 && i <= Math.pow(2, qc)) {
-				i++;
-
-				double k = c/((4.344 * i - 16.28) + (i/(-2.282 - 0.273 * i)) * c + 0.2407 * Math.log(i + 42.56));
-				double l = (1.2592 + 1.513 * i) * Math.tan(1.234 * Math.pow(i, -0.9907) * c);
+			double Rant = 0;
+			double N = 0;
+			double R = 0;
+			int c, s;
+			double l, k;
+			double pot = Math.pow(2, qc);
+			//System.out.println("Limit is= "+ (int) Math.pow(2, qc));
+			while(qn == -1 && i < (int)pot) {	
+				i++;		
+				c = 0;
+				s = 0;
+				//System.out.println("Fetching slot number "+ (i-1));
+				if (frame[i-1] > 1){
+					c = 1;
+				} else if (frame[i-1] == 1) {
+					s = 1;
+				}
+				k = c/((4.344 * i - 16.28) + (i/(-2.282 - 0.273 * i)) * c + 0.2407 * Math.log(i + 42.56));
+				l = (1.2592 + 1.513 * i) * Math.tan((Math.pow(1.234 *i, -0.9907)) * c);
 
 				if (k < 0) k = 0;
 
-				double N = _N(k, s, l);
-				double R = _R(N, qc, i);
+				N = (k*s) + l;
+				R = (N*pot)/i;
+				
+				/*if (c==0){
+					R = (s*pot)/i;
+				}*/
 				
 				if (i > 1) {
-					double L1 = Math.pow(2, qc);
+					double L1 = pot;
 					double L2 = Math.pow(2, Math.round(log2(R)));
 					double ps1 = (R/L1) * Math.pow((1 - (1/L1)), R-1);
 					double ps2 = (R/L2) * Math.pow((1 - (1/L2)), R-1);
 
-					if((R - _R(N,qc,i-1)) <= 1 && (L1 * ps1-s) < (L2 * ps2))
-						qn = log2(R);
+					if((R - Rant) <= 1 && ((L1 * ps1-s) < (L2 * ps2))){
+						qn = (int) Math.ceil(log2(R));
+					}
 				}
 				
 			}
+			Rant = R;
 			if (qn != -1) return Math.pow(2, qn);
 			else {
 				qn = qc;
@@ -173,6 +198,6 @@ public class Simulator {
 
 	public static void main(String[] args) {
 		Simulator simulator = new Simulator();
-		simulator.simulate("ilcm-sbs", 100, 100, 1000, 1000, 64);
+		simulator.simulate("ilcm-sbs", 1, 50, 251, 10000, 128);
 	}
 }
